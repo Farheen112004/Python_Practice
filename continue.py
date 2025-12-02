f03b37cd46c8d1d530f1029137ce00aac77b1f25
Farheen112004 <farheenfairy2004@gmail>
2025-12-02 13:36:41 +0530

    loopscommit

---
+++ continue.py
@@ -0,0 +1,5 @@
+for letter in 'geeksforgeeks':
+    if letter == 'e' or letter == 'r':  #skips these stuff
+        continue
+    print('Current Letter :', letter)
+ 
---
+++ pass.py
@@ -0,0 +1,4 @@
+for letter in 'geeksforgeeks':
+    pass
+print('Last Letter :', letter)
+#We use pass statement in Python to write empty loops. Pass is also used for empty control statements, functions and classes.
