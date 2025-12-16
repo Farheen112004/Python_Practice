7c396803c08d1f13189fbb739fad45681c5c501e
Farheen112004 <farheenfairy2004@gmail>
2025-12-16 10:59:02 +0530

    c

--- dict_loop.py
+++ dict_loop.py
@@ -1,3 +1,7 @@
-knights = {'gallahad': 'the pure', 'robin': 'the brave'}
-for k, v in knights.items(): #items is used for displaying both key and value
-    print(k, v)
+# knights = {'gallahad': 'the pure', 'robin': 'the brave'}
+# for k, v in knights.items(): #items is used for displaying both key and value
+#     print(k, v)
+
+d = dict({'x':'far', 'y':'s'})
+for x in d:
+    print("%s  %s" % (x, d[x]))
--- dictionaries.py
+++ dictionaries.py
@@ -12,3 +12,5 @@ print(sorted(a))
 
 a= dict(sape=4139, guido=4127, jack=4098)
 print(a)
+
+
